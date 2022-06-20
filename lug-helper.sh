e7a3b30564d5e9904f38c2e5ad5e67ab805533d3
Termuellinator <matmue@gmx.de>
2022-06-20 14:44:42 +0200

    check if zstd is installed

--- lug-helper.sh
+++ lug-helper.sh
@@ -65,6 +65,12 @@ if [ ! -x "$(command -v curl)" ]; then
     notify-send "lug-helper" "The required package 'curl' was not found on this system.\n" --icon=dialog-warning
     exit 1
 fi
+if [ ! -x "$(command -v zstd)" ]; then
+# Print to stderr and also try warning the user through notify-send
+    printf "lug-helper.sh: The  package 'zstd' was not found on this system. You won't be able to install Runners from TKG.\n" 1>&2
+    notify-send "lug-helper" "The package 'zstd' was not found on this system. You won't be able to install Runners from TKG.\n" --icon=dialog-warning
+    exit 1
+fi
 if [ ! -x "$(command -v mktemp)" ] || [ ! -x "$(command -v sort)" ] || [ ! -x "$(command -v basename)" ] || [ ! -x "$(command -v realpath)" ] || [ ! -x "$(command -v dirname)" ]; then
     # Print to stderr and also try warning the user through notify-send
     printf "lug-helper.sh: One or more required packages were not found on this system.\nPlease check that the following coreutils packages are installed:\n- mktemp\n- sort\n- basename\n- realpath\n- dirname\n" 1>&2
