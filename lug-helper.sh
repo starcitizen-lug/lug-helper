ea4e88d5b203b0e1da5e189deb9aab7e57e1549e
the-sane <3657071+the-sane@users.noreply.github.com>
2022-11-14 01:11:02 -0500

    bump version

--- lug-helper.sh
+++ lug-helper.sh
@@ -190,7 +190,7 @@ lug_wiki="https://github.com/starcitizen-lug/information-howtos/wiki"
 # Github repo and script version info
 repo="starcitizen-lug/lug-helper"
 releases_url="https://github.com/$repo/releases"
-current_version="v2.0"
+current_version="v2.1"
 
 ############################################################################
 ############################################################################
