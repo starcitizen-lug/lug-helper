3d6eee99d13fb8d2ad919407ace3e00273512f78
the-sane <3657071+the-sane@users.noreply.github.com>
2023-03-17 19:33:58 -0400

    Update main menu header

--- lug-helper.sh
+++ lug-helper.sh
@@ -2471,8 +2471,8 @@ fi
 # Loop the main menu until the user selects quit
 while true; do
     # Configure the menu
-    menu_text_zenity="<b><big>Welcome, Space Penguin, to the Star Citizen LUG Helper!</big>\n\nThis Helper is designed to help optimize your system for Star Citizen</b>\n\nYou may choose from the following options:"
-    menu_text_terminal="Welcome, Space Penguin, to the Star Citizen Linux Users Group Helper!\n\nThis Helper is designed to help optimize your system for Star Citizen\nYou may choose from the following options:"
+    menu_text_zenity="<b><big>Greetings, Space Penguin!</big>\n\nThis tool is provided by the Star Citizen Linux Users Group</b>\n\nYou may choose from the following options:"
+    menu_text_terminal="Greetings, Space Penguin!\n\nThis tool is provided by the Star Citizen Linux Users Group\nYou may choose from the following options:"
     menu_text_height="140"
     menu_type="radiolist"
 
