3329f199e5ecae179400d3bb3069b53e6701e7fc
the-sane <3657071+the-sane@users.noreply.github.com>
2023-03-24 18:10:37 -0400

    Rename install and prelaunch scripts

--- lug-helper.sh
+++ lug-helper.sh
@@ -155,10 +155,10 @@ fi
 
 # Use Lutris install script installed by a packaged version of this script if available
 # Otherwise, default to the json in the same directory
-if [ -f "/usr/share/lug-helper/lug-lutris-install.json" ]; then
-    install_script="/usr/share/lug-helper/lug-lutris-install.json"
+if [ -f "/usr/share/lug-helper/lutris-sc-install.json" ]; then
+    install_script="/usr/share/lug-helper/lutris-sc-install.json"
 else
-    install_script="$helper_dir/lug-lutris-install.json"
+    install_script="$helper_dir/lutris-sc-install.json"
 fi
 
 ######## Runners ###########################################################
