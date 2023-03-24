d4570b1a9a99c36b281acc2bd3e9cb57b4504886
the-sane <3657071+the-sane@users.noreply.github.com>
2023-03-24 17:53:22 -0400

    Add comments

--- sc-prelaunch.sh
+++ sc-prelaunch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+############################################
+## Lutris pre-launch script for Star Citizen
+############################################
 
+## EAC Workaround: Remove EAC cache
 EACDIR="$WINEPREFIX/drive_c/users/$USER/AppData/Roaming/EasyAntiCheat"
 
 if [ -d "$EACDIR" ]; then
