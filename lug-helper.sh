3fc191102b662c13566f0aaba540ab7d08033f0d
the-sane <3657071+the-sane@users.noreply.github.com>
2023-04-21 13:16:29 -0400

    Work around gitlab api nonsense
    direct_asset_url is no longer a direct url

--- lug-helper.sh
+++ lug-helper.sh
@@ -1389,7 +1389,7 @@ download_install() {
     fi
 
     # Get the selected download url
-    download_url="$(curl -s "$contributor_url$query_string" | grep -Eo "\"$search_key\": ?\"[^\"]+$download_file\"" | cut -d '"' -f4)"
+    download_url="$(curl -s "$contributor_url$query_string" | grep -Eo "\"$search_key\": ?\"[^\"]+$download_file\"" | cut -d '"' -f4 | sed 's|/-/blob/|/-/raw/|')"
 
     # Sanity check
     if [ -z "$download_url" ]; then
