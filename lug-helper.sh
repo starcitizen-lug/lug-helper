8d621bdf18ff34f0721f81007798a05618b5f823
the-sane <3657071+the-sane@users.noreply.github.com>
2023-12-26 13:45:05 -0500

    bump version

--- lug-helper.sh
+++ lug-helper.sh
@@ -223,7 +223,7 @@ lug_wiki="https://starcitizen-lug.github.io"
 # Github repo and script version info
 repo="starcitizen-lug/lug-helper"
 releases_url="https://github.com/$repo/releases"
-current_version="v2.11"
+current_version="v2.12"
 
 # NixOS section in Wiki
 lug_wiki_nixos="https://github.com/starcitizen-lug/knowledge-base/wiki/Tips-and-Tricks#nixos-tweaks"
