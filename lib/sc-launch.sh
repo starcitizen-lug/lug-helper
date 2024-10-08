713f9e49d1aafafe8b35b392d75ba1ba7fbc9e38
the-sane <3657071+the-sane@users.noreply.github.com>
2024-10-08 00:01:25 -0400

    Cut down on console debug messages

--- lib/sc-launch.sh
+++ lib/sc-launch.sh
@@ -20,6 +20,7 @@
 #####################################################
 export WINEPREFIX="$HOME/Games/star-citizen"
 export WINEDLLOVERRIDES=winemenubuilder.exe=d # Prevent updates from overwriting our .desktop entries
+export WINEDEBUG=-all # Cut down on console debug messages
 #export EOS_USE_ANTICHEATCLIENTNULL=1
 export __GL_SHADER_DISK_CACHE=1
 export __GL_SHADER_DISK_CACHE_SIZE=1073741824
