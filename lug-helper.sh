55207a90aec9ee89fb89dee12b342cf3709ab803
the-sane <3657071+the-sane@users.noreply.github.com>
2024-10-26 15:56:16 -0400

    bump version

--- lug-helper.sh
+++ lug-helper.sh
@@ -231,7 +231,7 @@ winetricks_url="https://raw.githubusercontent.com/Winetricks/winetricks/master/s
 # Github repo and script version info
 repo="starcitizen-lug/lug-helper"
 releases_url="https://github.com/$repo/releases"
-current_version="v3.0.1"
+current_version="v3.1"
 
 ############################################################################
 ############################################################################
