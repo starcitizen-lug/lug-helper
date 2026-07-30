fca474e09c6fea45ff5005f860ab6ee96cf73037
the-sane <3657071+the-sane@users.noreply.github.com>
2026-07-30 15:07:02 -0400

    bump version

--- lug-helper.sh
+++ lug-helper.sh
@@ -185,7 +185,7 @@ rsi_installer_latest_yml="${rsi_installer_base_url}/latest.yml"
 repo="starcitizen-lug/lug-helper"
 git_url="https://github.com/${repo}"
 releases_url="${git_url}/releases"
-current_version="v4.14"
+current_version="v4.15"
 changelog_url="${releases_url}/tag/${current_version}"
 
 ############################################################################
